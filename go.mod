39db661c17d171b81b94ec6749465e56fb112205
minchao <minchao.220@gmail.com>
2019-01-07 08:49:33 +0800

    Implement the api to get companies by keyword

--- go.mod
+++ go.mod
@@ -5,5 +5,10 @@ require (
 	github.com/awslabs/aws-lambda-go-api-proxy v0.2.0
 	github.com/go-chi/chi v3.3.3+incompatible
 	github.com/go-chi/render v1.0.1
+	github.com/go-playground/form v3.1.3+incompatible
+	github.com/go-playground/locales v0.12.1 // indirect
+	github.com/go-playground/universal-translator v0.16.0 // indirect
+	github.com/leodido/go-urn v1.1.0 // indirect
 	github.com/minchao/go-gcis v0.0.0-20181216042317-3dc7b0995a78
+	gopkg.in/go-playground/validator.v9 v9.24.0
 )
--- internal/app/company/handler.go
+++ internal/app/company/handler.go
@@ -11,7 +11,9 @@ import (
 	"github.com/awslabs/aws-lambda-go-api-proxy/chi"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+	"github.com/go-playground/form"
 	"github.com/minchao/go-gcis/gcis"
+	"gopkg.in/go-playground/validator.v9"
 )
 
 var (
@@ -22,6 +24,7 @@ var (
 func Handler(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	if chiAdapter == nil {
 		r := chi.NewRouter()
+		r.Get("/companies", getCompaniesByKeyword)
 		r.Get("/companies/{id:[0-9]+}", getCompany)
 
 		chiAdapter = chiadapter.New(r)
@@ -61,4 +64,67 @@ func getCompany(w http.ResponseWriter, r *http.Request) {
 		ID:   info.BusinessAccountingNO,
 		Name: info.CompanyName,
 	})
+}
+
+type companiesResponse struct {
+	Data []*company `json:"data"`
+}
+
+func (c *companiesResponse) Render(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+type companiesByKeywordOptions struct {
+	Keyword string `form:"keyword" validate:"required"`
+	Status  string `form:"status"`
+	Limit   int    `form:"limit" validate:"gt=0"`
+	Offset  int    `form:"offset"`
+}
+
+func (opt *companiesByKeywordOptions) isValid() error {
+	return validator.New().Struct(opt)
+}
+
+func getCompaniesByKeyword(w http.ResponseWriter, r *http.Request) {
+	var opt companiesByKeywordOptions
+	err := form.NewDecoder().Decode(&opt, r.URL.Query())
+	if err != nil {
+		render.Status(r, http.StatusBadRequest)
+		_ = render.Render(w, r, &restutil.ErrorResponse{Message: http.StatusText(http.StatusBadRequest)})
+		return
+	}
+	if err := opt.isValid(); err != nil {
+		render.Status(r, http.StatusUnprocessableEntity)
+		_ = render.Render(w, r, &restutil.ErrorResponse{Message: http.StatusText(http.StatusUnprocessableEntity)})
+		return
+	}
+	if opt.Status == "" {
+		opt.Status = "01"
+	}
+
+	list := make([]*company, 0)
+
+	out, _, err := client.Company.SearchByKeyword(context.Background(),
+		&gcis.CompanyByKeywordInput{
+			CompanyName:   opt.Keyword,
+			CompanyStatus: opt.Status,
+			SearchOptions: gcis.SearchOptions{
+				Top:  opt.Limit,
+				Skip: opt.Offset,
+			},
+		})
+	if err != nil {
+		render.Status(r, http.StatusInternalServerError)
+		_ = render.Render(w, r, &restutil.ErrorResponse{Message: "Unexpected error"})
+		return
+	}
+	for _, info := range out {
+		c := &company{
+			ID:   info.BusinessAccountingNO,
+			Name: info.CompanyName,
+		}
+		list = append(list, c)
+	}
+
+	_ = render.Render(w, r, &companiesResponse{Data: list})
 }
